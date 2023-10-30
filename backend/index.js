a514dbaee6295d2b1347bab43b8c9ebdaa33f9cc
anshalchauhan <59960661+anshalchauhan@users.noreply.github.com>
2023-10-30 19:12:44 +0530

    cors 2nd commit

--- backend/index.js
+++ backend/index.js
@@ -40,8 +40,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:3000",
-    origin: "*",
+    origin: "http://localhost:3000 https://gochat-kbxs.onrender.com",
     methods: ["GET", "POST"],
   },
 });
