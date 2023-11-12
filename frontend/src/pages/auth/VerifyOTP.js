13640c0a10588be01cc57ed6aa64db952aa0a1c2
anshalchauhan <59960661+anshalchauhan@users.noreply.github.com>
2023-11-12 18:23:28 +0530

    latest commit

--- frontend/src/pages/auth/VerifyOTP.js
+++ frontend/src/pages/auth/VerifyOTP.js
@@ -5,16 +5,17 @@ import React from "react";
 import { Stack, Typography } from "@mui/material";
 import VerifyOTPForm from "../../sections/auth/VerifyOTPForm";
 
+// React-Redux
+import { useSelector } from "react-redux";
+
 const VerifyOTP = () => {
+  const { email } = useSelector((state) => state.auth);
   return (
     <>
       <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
         <Typography variant="h4">Please Verfiy OTP</Typography>
         <Stack direction="row" spacing={0.5}>
-          <Typography variant="body2">
-            Sent to Email
-            {/* (anshalchauhan889@gmail.com) */}
-          </Typography>
+          <Typography variant="body2">{`Sent to Email (${email})`}</Typography>
         </Stack>
       </Stack>
       {/* Verify Form */}
